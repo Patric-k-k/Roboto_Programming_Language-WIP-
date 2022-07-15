f78c29b1b47fb49979d465aa7216a6503d3863d6
Patric-k-k <beepbooppk@gmail.com>
2022-07-15 14:16:20 -0500

    Created Repository

---
+++ Roboto.py
@@ -0,0 +1,130 @@
+# COLORS
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+#ERRORS
+class Error:
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        result += f'\n File: {self.pos_start.fn}, line: {self.pos_start.ln + 1}'
+        return result
+class IllegalCharError:
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__("File:" + str(pos_start) + " Line:" + str(pos_end) + 'Illegal Character Error', str(details))
+# CONSTATS
+
+DIGITS = '0123456789'
+
+# POSISION
+class POSITION:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+        if current_char == '\n':
+            self.ln += 1
+            self.col += 1
+        return self
+    def copy(self):
+        return POSITION(self.idx, self.ln, self.col, self.fn, self.ftxt)
+# TOKENS
+TT_INT = 'TT_INT'
+TT_FLOAT = 'TT_FLOAT'
+TT_PLUS = 'TT_PLUS'
+TT_MINUS = 'TT_MINUS'
+TT_MUL = 'TT_MUL'
+TT_DIV = 'TT_DIV'
+TT_LPAREN = 'TT_LPAREN'
+TT_RPAREN = 'TT_RPAREN'
+class Token:
+    def __init__(self, type_, value=None):
+        self.type = type_
+        self.value = value
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+# Lexer
+class Lexer:
+    def __init__(self, fn, text):
+        self.fn = fn 
+        self.text = text
+        self.pos = POSITION(-1, 0, -1, fn, text)
+        self.current_char = None
+        self.advance()
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char is not None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+            else:
+                pos_start = self.pos.copy()
+
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(str(pos_start), str(self.pos), '"'+ str(char) +'"')
+        return tokens, None
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
+        while self.current_char is not None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+        
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
+
+# RUN
+def run(fn, text):
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    return tokens, error
---
+++ Shell.py
@@ -0,0 +1,9 @@
+import Roboto
+from Roboto import bcolors as Colors
+while True:
+    text = input("$ ")
+    result, error = Roboto.run("User", text)
+    if error:
+        print(Colors.FAIL + error.as_string() + Colors.ENDC)
+    else:
+        print(result)
